1623968b80b2c2b61b28792008523b9234b1af7b
tomytz <chr.tzifris@gmail.com>
2022-10-28 23:03:19 +0300

    Htan poly xrhsimh ayth h allagh

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello { 
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+        System.out.print("Ta legame");
+        System.out.print("Ta legame ksana");
+    }
+}
