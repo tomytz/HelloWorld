ffc1685a4d679f3dd609db186ce6df045fd04b78
tomytz <chr.tzifris@gmail.com>
2022-11-16 20:32:30 +0200

    Htan poly xrhsimh ayth h allagh 5

--- HelloWorld/Hello.java
+++ HelloWorld/Hello.java
@@ -1,7 +1,9 @@
-public class Hello { 
+package HelloWorld.HelloWorld;
+
+public class Hello {
     public static void main(String[] args) {
         System.out.print("Hello World");
         System.out.print("Ta legame");
-        System.out.print("Ti na sou pw kai ti peis");
+        System.out.print("Ti na sou pw kai ti peis kai ti na peis ksana");
     }
 }
